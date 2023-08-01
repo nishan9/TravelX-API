0b10320287622c06149d45989cc6b2e5f80aa1cd
nishan9 <nishan9@icloud.com>
2023-08-01 18:42:20 +0100

    Add Auth0 Logic

--- src/main/java/com/app/travelx/ApiController.java
+++ src/main/java/com/app/travelx/ApiController.java
@@ -2,14 +2,20 @@ package com.app.travelx;
 import com.amadeus.exceptions.ResponseException;
 import com.amadeus.resources.Location;
 import com.amadeus.resources.FlightOfferSearch;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 
 @RestController
 @CrossOrigin
-@RequestMapping(value="/api")
+@RequestMapping(value="api", produces = MediaType.APPLICATION_JSON_VALUE)
 
 public class ApiController {
+
+	@GetMapping(value = "/private")
+	public Message privateEndpoint() {
+		return new Message("All good. You can see this because you are Authenticated.");
+	}
 
 	@GetMapping("/locations")
 	public ArrayList<Suggestions> locations(@RequestParam(required=true) String keyword) throws ResponseException {
