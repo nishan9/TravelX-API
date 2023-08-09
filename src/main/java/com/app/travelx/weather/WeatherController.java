9372661159b5d5a54d54ac164bf54088b5a07609
ghooton2 <gghooton@gmail.com>
2023-08-09 22:11:27 +0100

    weather formatted nicely

--- src/main/java/com/app/travelx/weather/WeatherController.java
+++ src/main/java/com/app/travelx/weather/WeatherController.java
@@ -1,46 +1,64 @@
 package com.app.travelx.weather;
+import com.amadeus.exceptions.ResponseException;
+import com.amadeus.resources.Location;
+import com.app.travelx.amadeus.SuggestionsModel;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Set;
 
 @RestController
 @RequestMapping("/api/weather")
 public class WeatherController {
 
     @Autowired
-    private WeatherService weatherService;
+    private WeatherService service;
 
-
-    @GetMapping
-    public JSONObject getWeather(@RequestParam String cityName) {
-        return weatherService.getWeather(cityName);
+    @GetMapping()
+    public ResponseEntity<WeatherModel> weather(@RequestParam(required = true) String latitude,
+    //public JSONArray weather(@RequestParam(required = true) String latitude,
+                               @RequestParam(required = true) String longitude,
+                               @RequestParam(required = true) String date) throws ResponseException {
+        //int year = (int) date.substring(0,4);
+        return service.getWeather(latitude, longitude, date);
     }
 
-    @GetMapping("/weatherArray")
-    public JSONArray getWeatherArray(@RequestParam String cityName) {
-        JSONObject weatherData = weatherService.getWeather(cityName);
-        return weatherService.extractWeatherArray(weatherData);
-    }
 
-    @GetMapping("/main")
-    public JSONObject getMain(@RequestParam String cityName) {
-        JSONObject weatherData = weatherService.getWeather(cityName);
-        return weatherService.extractMain(weatherData);
-    }
 
-    @GetMapping("/sys")
-    public JSONObject getSys(@RequestParam String cityName) {
-        JSONObject weatherData = weatherService.getWeather(cityName);
-        return weatherService.extractSys(weatherData);
-    }
-
-    @GetMapping("/wind")
-    public JSONObject getWind(@RequestParam String cityName) {
-        JSONObject weatherData = weatherService.getWeather(cityName);
-        return weatherService.extractWind(weatherData);
-    }
+//    @GetMapping
+//    public JSONObject getWeather(@RequestParam String cityName) {
+//        return weatherService.getWeather(cityName);
+//    }
+//
+//    @GetMapping("/weatherArray")
+//    public JSONArray getWeatherArray(@RequestParam String cityName) {
+//        JSONObject weatherData = weatherService.getWeather(cityName);
+//        return weatherService.extractWeatherArray(weatherData);
+//    }
+//
+//    @GetMapping("/main")
+//    public JSONObject getMain(@RequestParam String cityName) {
+//        JSONObject weatherData = weatherService.getWeather(cityName);
+//        return weatherService.extractMain(weatherData);
+//    }
+//
+//    @GetMapping("/sys")
+//    public JSONObject getSys(@RequestParam String cityName) {
+//        JSONObject weatherData = weatherService.getWeather(cityName);
+//        return weatherService.extractSys(weatherData);
+//    }
+//
+//    @GetMapping("/wind")
+//    public JSONObject getWind(@RequestParam String cityName) {
+//        JSONObject weatherData = weatherService.getWeather(cityName);
+//        return weatherService.extractWind(weatherData);
+//    }
 }
--- src/main/java/com/app/travelx/weather/WeatherService.java
+++ src/main/java/com/app/travelx/weather/WeatherService.java
@@ -1,35 +1,72 @@
 package com.app.travelx.weather;
+import org.json.JSONArray;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-import org.json.JSONArray;
 import org.json.JSONObject;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.TextStyle;
+import java.time.temporal.ChronoUnit;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+//import java.util.ArrayList;
 
 @Service
 public class WeatherService {
-
     private final RestTemplate restTemplate = new RestTemplate();
-    private final String apiKey = "f9ec786fc529e0b49f6e586ba83a6f1e";
-    private final String api16 = "ae27cceb2c1eb3580afb151804acfeb8";
+    //public JSONArray getWeather(String latitude, String longitude, String date){
+    public ResponseEntity<WeatherModel> getWeather(String latitude, String longitude, String date){
+        if (checkDateFormat(date) && isDateWithin16Days(date)){
+             String url = "https://api.open-meteo.com/v1/forecast?latitude=" + latitude + "&longitude=" + longitude +
+                    "&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min," +
+                    "sunrise,sunset,uv_index_max,precipitation_sum,rain_sum,snowfall_sum,precipitation_hours," +
+                    "precipitation_probability_max,windspeed_10m_max&timezone=GMT&start_date=" + date + "&end_date=" + date;
+            String response = restTemplate.getForObject(url, String.class);
+            JSONObject all = new JSONObject(response);
 
-    public JSONObject getWeather(String cityName) {
-        String url = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=metric"  + "&appid=" + apiKey;
-        String response = restTemplate.getForObject(url, String.class);
-        return new JSONObject(response);
+            JSONObject daily = all.getJSONObject("daily");
+
+            int summary = daily.getJSONArray("weathercode").getInt(0);
+            float maxTemp = daily.getJSONArray("temperature_2m_max").getFloat(0);
+            float minTemp = daily.getJSONArray("temperature_2m_min").getFloat(0);
+            float maxApparentTemp = daily.getJSONArray("apparent_temperature_max").getFloat(0);
+            float minApparentTemp = daily.getJSONArray("apparent_temperature_min").getFloat(0);
+            float uvMax = daily.getJSONArray("uv_index_max").getFloat(0);
+            int rainSum = daily.getJSONArray("rain_sum").getInt(0);
+            int precipitationProbMax = daily.getJSONArray("precipitation_probability_max").getInt(0);
+            float snowSum = daily.getJSONArray("snowfall_sum").getFloat(0);
+            String sunrise = daily.getJSONArray("sunrise").getString(0);
+            String sunset = daily.getJSONArray("sunset").getString(0);
+
+            WeatherModel weatherModel = new WeatherModel(summary, maxTemp, minTemp, maxApparentTemp, minApparentTemp,
+                    uvMax, rainSum, precipitationProbMax, snowSum, sunrise, sunset);
+            return new ResponseEntity<> (weatherModel, HttpStatus.OK);
+        }else{
+            return null;
+        }
     }
 
-    public JSONArray extractWeatherArray(JSONObject weatherData) {
-        return weatherData.getJSONArray("weather");
+    public boolean checkDateFormat(String inputDate) {
+        String regex = "\\d{4}-\\d{2}-\\d{2}";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(inputDate);
+
+        return matcher.matches();
     }
 
-    public JSONObject extractMain(JSONObject weatherData) {
-        return weatherData.getJSONObject("main");
+    public boolean isDateWithin16Days(String inputDate) {
+        LocalDate currentDate = LocalDate.now();
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDate parsedDate = LocalDate.parse(inputDate, formatter);
+
+        long daysDifference = ChronoUnit.DAYS.between(currentDate, parsedDate);
+
+        return Math.abs(daysDifference) <= 16;
     }
 
-    public JSONObject extractSys(JSONObject weatherData) {
-        return weatherData.getJSONObject("sys");
-    }
-
-    public JSONObject extractWind(JSONObject weatherData) {
-        return weatherData.getJSONObject("wind");
-    }
 }
