92b981b9529cf0ffc1b1fac803c85ed96d54924f
nishan9 <47658117+nishan9@users.noreply.github.com>
2023-08-10 14:09:53 +0100

    Refactored to include isPaid

---
+++ src/main/java/com/app/travelx/database/bookings/BookingController.java
@@ -0,0 +1,9 @@
+package com.app.travelx.database.bookings;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+public class BookingController {
+}
---
+++ src/main/java/com/app/travelx/database/users/ChangeIsPaidModel.java
@@ -0,0 +1,9 @@
+package com.app.travelx.database.users;
+import java.util.List;
+
+public class ChangeIsPaidModel {
+
+    public String auth0id;
+    public List<Integer> BookingIDs;
+
+}
