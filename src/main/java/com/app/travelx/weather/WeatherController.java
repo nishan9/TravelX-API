50f4ccf9ba8d5b282237044567065123e93e8736
k-wadwa <kamal_wadwa@hotmail.com>
2023-08-13 14:26:26 +0100

    incorporated Mockito testing for updated WeatherService class, added more error handling through this

--- src/main/java/com/app/travelx/weather/WeatherController.java
+++ src/main/java/com/app/travelx/weather/WeatherController.java
@@ -1,13 +1,12 @@
 package com.app.travelx.weather;
+
 import com.amadeus.exceptions.ResponseException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 
 @RestController
 @RequestMapping("/api/weather")
@@ -18,9 +17,8 @@ public class WeatherController {
 
     @GetMapping()
     public ResponseEntity<WeatherModel> weather(@RequestParam(required = true) String latitude,
-                                                @RequestParam(required = true) String longitude,
-                                                @RequestParam(required = true) String date) throws ResponseException {
+            @RequestParam(required = true) String longitude,
+            @RequestParam(required = true) String date) throws ResponseException {
         return service.getWeather(latitude, longitude, date);
     }
 }
-
--- src/main/java/com/app/travelx/weather/WeatherModel.java
+++ src/main/java/com/app/travelx/weather/WeatherModel.java
@@ -6,6 +6,7 @@ import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
 public class WeatherModel {
     private String summary;
     private float maxTemp;
@@ -19,7 +20,8 @@ public class WeatherModel {
     private String sunrise;
     private String sunset;
 
-    public WeatherModel(String summary, float maxTemp, float minTemp, float maxApparentTemp, float minApparentTemp, float uvMax, float rainSum, int precipitationProbMax, float snowSum, String sunrise, String sunset) {
+    public WeatherModel(String summary, float maxTemp, float minTemp, float maxApparentTemp, float minApparentTemp,
+            float uvMax, float rainSum, int precipitationProbMax, float snowSum, String sunrise, String sunset) {
         this.summary = summary;
         this.maxTemp = maxTemp;
         this.minTemp = minTemp;
@@ -30,6 +32,94 @@ public class WeatherModel {
         this.precipitationProbMax = precipitationProbMax;
         this.snowSum = snowSum;
         this.sunrise = sunrise;
+        this.sunset = sunset;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public float getMaxTemp() {
+        return maxTemp;
+    }
+
+    public void setMaxTemp(float maxTemp) {
+        this.maxTemp = maxTemp;
+    }
+
+    public float getMinTemp() {
+        return minTemp;
+    }
+
+    public void setMinTemp(float minTemp) {
+        this.minTemp = minTemp;
+    }
+
+    public float getMaxApparentTemp() {
+        return maxApparentTemp;
+    }
+
+    public void setMaxApparentTemp(float maxApparentTemp) {
+        this.maxApparentTemp = maxApparentTemp;
+    }
+
+    public float getMinApparentTemp() {
+        return minApparentTemp;
+    }
+
+    public void setMinApparentTemp(float minApparentTemp) {
+        this.minApparentTemp = minApparentTemp;
+    }
+
+    public float getUvMax() {
+        return uvMax;
+    }
+
+    public void setUvMax(float uvMax) {
+        this.uvMax = uvMax;
+    }
+
+    public float getRainSum() {
+        return rainSum;
+    }
+
+    public void setRainSum(float rainSum) {
+        this.rainSum = rainSum;
+    }
+
+    public int getPrecipitationProbMax() {
+        return precipitationProbMax;
+    }
+
+    public void setPrecipitationProbMax(int precipitationProbMax) {
+        this.precipitationProbMax = precipitationProbMax;
+    }
+
+    public float getSnowSum() {
+        return snowSum;
+    }
+
+    public void setSnowSum(float snowSum) {
+        this.snowSum = snowSum;
+    }
+
+    public String getSunrise() {
+        return sunrise;
+    }
+
+    public void setSunrise(String sunrise) {
+        this.sunrise = sunrise;
+    }
+
+    public String getSunset() {
+        return sunset;
+    }
+
+    public void setSunset(String sunset) {
         this.sunset = sunset;
     }
 }
