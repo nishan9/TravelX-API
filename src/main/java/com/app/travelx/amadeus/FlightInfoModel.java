16f79631ce43121f7a408d6ad38459c59ed4dd3f
ghooton2 <gghooton@gmail.com>
2023-08-13 16:25:16 +0100

    amended flight test, passed

--- src/main/java/com/app/travelx/amadeus/FlightInfoModel.java
+++ src/main/java/com/app/travelx/amadeus/FlightInfoModel.java
@@ -22,4 +22,76 @@ public class FlightInfoModel {
         this.availableSeats = availableSeats;
         this.duration = duration;
     }
+
+    public String getDepartLocation() {
+        return departLocation;
+    }
+
+    public void setDepartLocation(String departLocation) {
+        this.departLocation = departLocation;
+    }
+
+    public String getArrivalLocation() {
+        return arrivalLocation;
+    }
+
+    public void setArrivalLocation(String arrivalLocation) {
+        this.arrivalLocation = arrivalLocation;
+    }
+
+    public String getDepartTime() {
+        return departTime;
+    }
+
+    public void setDepartTime(String departTime) {
+        this.departTime = departTime;
+    }
+
+    public String getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public void setArrivalTime(String arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
+
+    public String getAirline() {
+        return airline;
+    }
+
+    public void setAirline(String airline) {
+        this.airline = airline;
+    }
+
+    public String getCurrerncy() {
+        return currerncy;
+    }
+
+    public void setCurrerncy(String currerncy) {
+        this.currerncy = currerncy;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public int getAvailableSeats() {
+        return availableSeats;
+    }
+
+    public void setAvailableSeats(int availableSeats) {
+        this.availableSeats = availableSeats;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
 }
--- src/main/java/com/app/travelx/amadeus/SuggestionsModel.java
+++ src/main/java/com/app/travelx/amadeus/SuggestionsModel.java
@@ -13,4 +13,44 @@ public class SuggestionsModel {
         this.latitude = latitude;
         this.longitude = longitude;
     }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
 }
