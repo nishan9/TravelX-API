34a3f4ac51f5e05c7f3ffbe08517e9ae9b85592b
nishan9 <47658117+nishan9@users.noreply.github.com>
2023-08-13 19:14:33 +0100

    fixed imports

--- src/main/java/com/app/travelx/amadeus/SuggestionsModel.java
+++ src/main/java/com/app/travelx/amadeus/SuggestionsModel.java
@@ -1,7 +1,9 @@
 package com.app.travelx.amadeus;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @AllArgsConstructor
 @NoArgsConstructor
